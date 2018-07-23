864c5ec9380c2d1dc4adc518b64268acf58fbf54
Felix Clark <mfclark3690@gmail.com>
2018-07-23 15:47:30 -0400

    adding random implementation for base

--- src/base.rs
+++ src/base.rs
@@ -1,6 +1,12 @@
 
 use std::fmt;
-    
+
+extern crate rand;
+use self::rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
+
 // mod base { // goes into the base namespace by default, just from the filename.
 
 // using DNA base pairs AGCT. T and C are always interchangeable in the last base of a codon.
@@ -27,12 +33,26 @@ impl fmt::Display for Base {
     }
 }
 
+impl Distribution<Base> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Base {
+        use self::Base::*;
+        match rng.gen_range(0, 4) {
+            0 => C,
+            1 => T,
+            2 => A,
+            3 => G,
+            _ => panic!("generated out-of-range number for random base"),
+        }
+    }
+}
+
 fn complement(base: Base) -> Base{
+    use self::Base::*;
     match base {
-        Base::C => Base::G,
-        Base::T => Base::A,
-        Base::A => Base::T,
-        Base::G => Base::C,
+        C => G,
+        T => A,
+        A => T,
+        G => C,
     }
 }
     
